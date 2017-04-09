70d6b1499cb12c719f5939a823f63df92ae45d04
Michael Habib <michaelhabib1868@gmail.com>
2017-04-09 16:36:19 -0700

    Add React Routing to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import Routes from './routes'
 
@@ -9,4 +9,4 @@ ReactDOM.render(
     routes={Routes}
   />,
   document.getElementById('root')
-);
+)
