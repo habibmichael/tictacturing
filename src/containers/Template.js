5ae3bb2de64e91923660cfc739465113a452c45b
Michael Habib <michaelhabib1868@gmail.com>
2017-04-09 17:03:16 -0700

    Add material ui to project

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,10 +1,15 @@
-import  React, {Component} from 'react'
+import  React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 class Template extends Component {
 
   render() {
     return (
-      <div>
+      <MuiThemeProvider>
+        <div>
         <header>
           <h1>TicTacTuring</h1>
         </header>
@@ -12,6 +17,8 @@ class Template extends Component {
           {this.props.children}
         </main>
       </div>
+      </MuiThemeProvider>
+      
     )
   }
 }
